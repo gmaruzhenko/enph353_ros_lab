218750161e8eebf601cc03f08daf934dedb7e679
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2019-09-18 19:28:36 -0700

    added processing

--- lab1.py
+++ lab1.py
@@ -25,13 +25,81 @@ class image_converter:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
-
-    # (rows,cols,channels) = cv_image.shape
-    # if cols > 60 and rows > 60 :
-    #   cv2.circle(cv_image, (50,50), 10, 255)
-
+    
     cv2.imshow("Image window", cv_image)
     cv2.waitKey(0)
+
+    process_image(cv_image)
+
+
+def process_image(image):
+    # frame count itterator setup
+    count = 0
+    success = True
+    while success:
+        #Convert to Grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        #Blur image to reduce noise. if Kernel_size is bigger the image will be more blurry
+        blurred = cv2.GaussianBlur(gray, (Kernel_size, Kernel_size), 0)
+
+        #debug to find size of image
+        # print blurred.shape
+
+        # crop down to last few slices (actually not needed)
+        crop_img = blurred[200:240, 0:320]
+
+        # cv2.imshow("cropped", crop_img)
+        # cv2.waitKey(0)
+
+        #Perform canny edge-detection.
+        #If a pixel gradient is higher than high_threshold is considered as an edge.
+        #if a pixel gradient is lower than low_threshold is is rejected , it is not an edge.
+        #Bigger high_threshold values will provoque to find less edges.
+        #Canny recommended ratio upper:lower  between 2:1 or 3:1
+        edged = cv2.Canny(crop_img, low_threshold, high_threshold)
+
+        # cv2.imshow("cropped", edged)
+        # cv2.waitKey(0)
+
+        # Note Black is 0 white is 1
+        # Find first and second occurance of contour plot on second to
+        # last row of pixel (approximation of real path)
+        first_white = 0
+        second_white = 0
+
+        #due to image size
+        index_max = 320
+        index = 0
+
+        while index<index_max:
+
+            if edged[39,index] > 0:
+                if first_white == 0:
+                    first_white = index
+                else:
+                    second_white = index
+            
+
+            index+=1
+
+        average_white = int((first_white+second_white)/2)
+
+        # print average_white
+
+        # Now draw circle showing the center location of our contour line
+        cv2.circle(image,(average_white,210),20,(0,0,255),1)
+
+        # run the slideshow at a min wait of 1 ms
+        cv2.imshow("cropped", image)
+        cv2.waitKey(1)
+
+        #reset image for next frame 
+
+        count += 1
+
+
+
 
 def main(args):
   ic = image_converter()
