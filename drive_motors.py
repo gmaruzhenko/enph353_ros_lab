73f7e26df42ac6b789da4a191d432ab881d604d1
Tyler Keeling <tylerwkeeling@gmail.com>
2019-09-25 17:33:54 -0700

    drive_motors node implemented Tested, supplies linear/angular velocity based on subscribed error

--- drive_motors.py
+++ drive_motors.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
+from __future__ import division
+
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Int16
+from geometry_msgs.msg import Twist
 
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %i", data.data)
-    
-def listener():
+class Motor_Driver:
+    def __init__(self):
+        self.gain = 1/2 
+        self.basespeed = 10
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+        rospy.init_node('motor_driver', anonymous=True)
+        rospy.Subscriber("/rrbot/cv_stear_error", Int16, self.callback)
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+            
 
-    rospy.Subscriber("/rrbot/cv_stear_error", Int16, callback)
-
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    def callback(self, data):
+        rospy.loginfo(data.data+self.gain)
+        s = Twist()
+        s.linear.x = self.basespeed
+        s.angular.z = data.data*self.gain
+        self.pub.publish(s)
+     
 
 if __name__ == '__main__':
-    listener()
+    driver = Motor_Driver()
+    rospy.spin()
